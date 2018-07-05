f52231a16c7e172dcafafc544dcc1c7ad6185159
Justin <theoperaking@yahoo.com>
2018-07-05 11:42:03 -0700

    element and boost query functional

---
+++ FFRK_Element_Info/ViewModels/BoostDisplayVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FFRK_Element_Info.ViewModels
+{
+    public class BoostDisplayVm
+    {
+        public string Name { get; set; }
+        public string Iteration { get; set; }
+        public string ExtraInfo { get; set; }
+    }
+}
---
+++ FFRK_Element_Info/ViewModels/ElementDisplayVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FFRK_Element_Info.ViewModels
+{
+    public class ElementDisplayVm
+    {
+        public string Name { get; set; }
+        public string ElementType { get; set; }
+        public string EnElement { get; set; }
+    }
+}
